96031a46020f13cb47f8aa2b0ab369c8f8a2a2df
zezinho <maralves1990@gmail.com>
2022-12-12 04:17:42 -0300

    finished

--- js/scripts.js
+++ js/scripts.js
@@ -147,4 +147,48 @@ $(document).ready(function(){
             });
         }
     }
+
+// scroll para seções 
+
+let navbtn = $('.nav-item');
+
+let bannerSection = $('#mainSlider');
+let aboutSection = $('#about-area');
+let serviceSection = $('#service-area');
+let teamSection = $('#team-area');
+let portifolioSection = $('#portifolio-area');
+let contactSection = $('#contact-area');
+
+let scrollTo = "";
+
+    $(navbtn).click(function(){
+
+        let btnId = $(this).attr('id');
+
+        if(btnId == "about-menu"){
+            scrollTo = aboutSection;
+        }
+        else if (btnId == "service-menu"){
+            scrollTo = serviceSection;
+        }
+        else if (btnId == "team-menu"){
+            scrollTo = teamSection;
+        }
+        else if (btnId == "portifolio-menu"){
+            scrollTo = portifolioSection;
+        }
+        else if (btnId == "contact-menu"){
+            scrollTo = contactSection;
+        }
+        else {
+            scrollTo = bannerSection;
+        }
+
+        $([document.documentElement, document.body]).animate({
+        
+            scrollTop: $(scrollTo).offset().top -70
+        }, 1500);
+    });
+
+    
 });
